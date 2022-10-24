b7baba7c7c8cf76055deb9c1b44dd34c28770442
Ducky <manwel.bugeja.18@um.edu.mt>
2022-10-24 11:16:17 +0200

    Translated multi-rule scannign script to python

--- scan.py
+++ scan.py
@@ -2,6 +2,11 @@ import os
 import sys
 import re
 
+def listfiles(folder):
+    for root, folders, files in os.walk(folder):
+        for filename in folders + files:
+            yield os.path.join(root, filename)
+
 if not os.path.isdir(sys.argv[1]):
     print('Give me report folder argv[1]')
     sys.exit()
@@ -10,10 +15,15 @@ reports = [f for f in os.listdir(sys.argv[1])]
 if not os.path.isdir(sys.argv[2]):
     print('Give me apk folder argv[2]')
 apks = [ f for f in os.listdir(sys.argv[2])]
-for report in reports:
-    apkname = re.findall('(.*)-report',report)[0]
-    if ' ' in report:
-        apkname = '\''+apkname+'\''
-        report = '\''+report+ '\''
-    os.system('yara -x androguard='+ '/'.join([sys.argv[1],report]) + ' ' + sys.argv[3] + ' ' +'/'.join([sys.argv[2],apkname])) 
 
+for filename in listfiles('./rules'):
+    match = re.search("\.yar$", filename)
+
+    # if match is found
+    if match:
+        for report in reports:
+            apkname = re.findall('(.*)-report',report)[0]
+            if ' ' in report:
+                apkname = '\''+apkname+'\''
+                report = '\''+report+ '\''
+            os.system('yara -x androguard='+ '/'.join([sys.argv[1],report]) + ' ' + filename + ' ' +'/'.join([sys.argv[2],apkname])) 
