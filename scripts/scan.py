a183a796bfee240c0b8c7579f8a6c659957b4fae
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-04-03 12:05:45 +0200

    changed baseline1 to original androrat

--- scripts/scan.py
+++ scripts/scan.py
@@ -11,6 +11,6 @@ output = sys.argv[3]
 tags = ["collection", "defense_evasion", "impact", "persistence", "c2"]
 
 for tag in tags:
-    cmd = f"yara -t {tag} {rules} {to_scan} > {os.path.join(output, f'{trimmed(to_scan)}_{tag}.txt')}"
+    cmd = f"yara -t {tag} {rules} -r {to_scan} > {os.path.join(output, f'{trimmed(to_scan)}_{tag}.txt')}"
     print(f"Running command: {cmd}")
     os.system(cmd)
